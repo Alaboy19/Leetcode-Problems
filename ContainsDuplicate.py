bd5c061e8321e898561e67735f1632864320005c
Alaboy19 <47283347+Alaboy19@users.noreply.github.com>
2023-07-06 11:14:35 +0600

    Create ContainsDuplicate.py

---
+++ ContainsDuplicate.py
@@ -0,0 +1,14 @@
+def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+
+        return False
+'''
+1) the idea is very simple
+2) we use hashset to store the seen elements while iterating
+3) we check whether it contains already in seen -> if yes -> return True, if not -> keep going and add this num in seen
+4) return defult False value if iteration reaches the end -> which means no duplate met 
+'''
