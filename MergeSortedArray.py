85223a3e0ec4cc645719645e15fe03c172620ec1
Alaboy19 <47283347+Alaboy19@users.noreply.github.com>
2023-07-06 12:26:34 +0600

    Create MergeSortedArray.py

---
+++ MergeSortedArray.py
@@ -0,0 +1,32 @@
+'''
+Given:
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+'''
+'''
+Solution:
+1) since both sorted, start looking for last index 
+2) compare last of two and fill the greatest, from which filled , -=1 pointer 
+3) if first finished and there left second one, prepend second one two first ones 
+'''
+
+def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
